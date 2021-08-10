b4a8137f24e8bde678da83b4bb8bac12843290ff
Gustavo Lima <glima.0913@gmail.com>
2021-08-10 16:42:31 -0300

    Finalização dos Scripts

--- SCRIPT/HROADS_02_DML.sql
+++ SCRIPT/HROADS_02_DML.sql
@@ -1,3 +1,36 @@
 --DML
 USE SENAI_HROADS_TARDE;
 GO
+
+INSERT INTO CLASSE (nomeClasse)
+VALUES ('Bárbaro'), ('Cruzado'), ('Caçadora de demônios'), ('Monge'), ('Necromante'), ('Feiticeiro'), ('Arcanista');
+GO
+
+INSERT INTO TIPOHABILIDADE (nomeTipoHabilidade)
+VALUES ('ATAQUE'), ('DEFESA'), ('CURA'), ('MAGIA'); 
+GO
+
+INSERT INTO HABILIDADE (idTipoHabilidade, nomeHabilidade)
+VALUES (1, 'Lança Mortal'), (2, 'Escudo Supremo'), (3, 'Recuperar Vida');
+GO
+
+INSERT INTO CLASSEHABILIDADE (idHabilidade, idClasse)
+VALUES (1, 1), (2, 1), (1, 2), (1, 3), (3, 4), (2, 4), (3, 6);
+GO 
+
+INSERT INTO PERSONAGEM (idClasse, nomePersonagem, capMaxVida, capMaxMana, dataAtualizacao, DataCriacao)
+VALUES (1, 'DeuBug', 100, 80, '10/08/2021', '18/01/2019'), (4, 'BitBug', 70, 100, '10/08/2021', '17/03/2016'), (7, 'Fer8', 75, 60, '10/08/2021', '18/03/2018');
+GO
+
+/* Atualizar o nome do personagem Fer8 para Fer7;
+UPDATE PERSONAGEM
+SET nomePersonagem = 'Fer7'
+WHERE idPersonagem = 4;
+GO
+
+Atualizar o nome da classe de Necromante para Necromancer:
+UPDATE CLASSE
+SET nomeClasse = 'Necromancer'
+WHERE IdClasse = 5;
+GO
+*/
--- SCRIPT/HROADS_03_DQL.sql
+++ SCRIPT/HROADS_03_DQL.sql
@@ -1,3 +1,70 @@
 --DQL
 USE SENAI_HROADS_TARDE;
 GO
+
+-- Selecionar todos os personagens:
+SELECT * FROM PERSONAGEM;
+GO
+
+-- Selecionar todas as classes:
+SELECT * FROM CLASSE;
+GO
+
+-- Selecionar somente o nome das classes:
+SELECT nomeClasse FROM CLASSE;
+GO
+
+-- Selecionar todas as habilidadees:
+SELECT * FROM HABILIDADE;
+GO
+
+-- Realizar a contagem de quantas habilidades estão cadastradas:
+SELECT COUNT(idHabilidade) FROM HABILIDADE;
+GO
+
+-- Selecionar somente os id’s das habilidades classificando-os por ordem crescente:
+SELECT idHabilidade FROM HABILIDADE
+ORDER BY idHabilidade ASC
+GO
+
+-- Selecionar todos os tipos de habilidades:
+SELECT * FROM TIPOHABILIDADE;
+GO
+
+-- Selecionar todas as habilidades e a quais tipos de habilidades elas fazem parte:
+SELECT * FROM HABILIDADE
+INNER JOIN TIPOHABILIDADE
+ON HABILIDADE.idTipoHabilidade = TIPOHABILIDADE.idTipoHabilidade;
+GO
+
+-- Selecionar todos os personagens e suas respectivas classes:
+SELECT * FROM PERSONAGEM 
+INNER JOIN CLASSE
+ON PERSONAGEM.idClasse = CLASSE.idClasse;
+GO
+
+-- Selecionar todos os personagens e as classes (mesmo que elas não tenham correspondência em personagens:
+SELECT * FROM PERSONAGEM
+RIGHT JOIN CLASSE
+ON PERSONAGEM.idClasse = CLASSE.idClasse;
+GO
+
+-- Selecionar todas as classes e suas respectivas habilidades:
+SELECT * FROM CLASSEHABILIDADE;
+GO
+
+-- Selecionar todas as habilidades e suas classes (somente as que possuem correspondência):
+SELECT nomeHabilidade Habilidade, nomeClasse Classe FROM HABILIDADE
+INNER JOIN CLASSEHABILIDADE
+ON HABILIDADE.idHabilidade = CLASSEHABILIDADE.idHabilidade
+INNER JOIN CLASSE
+ON CLASSEHABILIDADE.idClasse = CLASSE.idClasse
+GO
+
+-- Selecionar todas as habilidades e suas classes (mesmo que elas não tenham correspondência:
+SELECT nomeHabilidade Habilidade, nomeClasse Classe FROM HABILIDADE
+FULL OUTER JOIN CLASSEHABILIDADE
+ON HABILIDADE.idHabilidade = CLASSEHABILIDADE.idHabilidade
+FULL OUTER JOIN CLASSE
+ON CLASSEHABILIDADE.idClasse = CLASSE.idClasse
+GO
